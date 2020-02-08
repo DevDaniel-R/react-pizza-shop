c1a93358a48bcbd2e5cac184fcffaa9d96e211e3
Daniel Rodriguez <drodriguez661@gmail.com>
2020-02-08 17:51:22 -0600

    added a banner, navbar position

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createGlobalStyle } from 'styled-components';
 import { Navbar } from "./Navbar/Navbar";
+import { Banner } from "./Banner/Banner";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -19,7 +20,7 @@ function App() {
     <>
     <GlobalStyle/>
     <Navbar/>
-    <div> sliceline </div>
+    <Banner />
     </>
   );
 }
