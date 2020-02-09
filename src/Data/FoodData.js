9864fe28798c8154a952a8316276ce1fc5176888
Daniel Rodriguez <drodriguez661@gmail.com>
2020-02-09 16:03:28 -0600

    added food data and food gridd

---
+++ src/Data/FoodData.js
@@ -0,0 +1,31 @@
+export const foods = [
+  {
+    name: 'Cheese Pizza',
+    img: '/img/pizza.png'
+  },
+  {
+    name: 'Pepperoni Pizza',
+    img: '/img/pizza2.jpeg'
+  },
+  {
+    name: 'Veggie Pizza',
+    img: '/img/healthy-pizza.jpeg'
+  },
+  {
+    name: 'Chicken Pizza',
+    img: '/img/chicken-pizza.jpeg'
+  },
+  {
+    name: 'Wings',
+    img: '/img/wings.jpeg'
+  },
+  {
+    name: 'wedges',
+    img: '/img/wedges.jpg'
+  },
+  {
+    name: 'Cheesesticks',
+    img: '/img/cheesesticks.jpeg'
+  }
+
+];
