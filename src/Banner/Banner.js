e5dc0cfcc5a8dda7d6249ffd8eae9f2f3771a797
Daniel Rodriguez <drodriguez661@gmail.com>
2020-02-08 20:29:23 -0600

    add filter on banner

---
+++ src/Banner/Banner.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const Banner = styled.div`
+height: 60vh;
+background-image: url("img/pizza.jpg");
+background-position: center;
+background-size: cover;
+filter: contrast(75%);
+`
