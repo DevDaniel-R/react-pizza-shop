5d54bf314b7e89430986247b20e08fcfb73fa034
Daniel Rodriguez <drodriguez661@gmail.com>
2020-02-10 13:28:07 -0600

    FoodDialog functioning with Hooks

--- src/Banner/Banner.js
+++ src/Banner/Banner.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const Banner = styled.div`
-height: 60vh;
-background-image: url("img/pizza.jpg");
+height: 80vh;
+background-image: url("img/pizza.jpeg");
 background-position: center;
 background-size: cover;
 filter: contrast(75%);
